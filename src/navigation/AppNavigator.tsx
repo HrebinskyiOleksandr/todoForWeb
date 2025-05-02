78012f662ec8cdee10a911318eeb11ab3192fcfd
oleksandrgrebinskij <roki000sasha@gmail.com>
2025-05-02 13:25:19 +0300

    default settings and navigation

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {RouteKey} from "../core/typing/enums/navigator.ts";
+import {TodoList} from "../screens/TodoList.tsx";
+
+const Stack = createNativeStackNavigator();
+export const AppNavigator = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName='HomeScreen'
+      screenOptions={{headerShown: false}}
+    >
+      <Stack.Screen name={RouteKey.TodoList} component={TodoList}/>
+    </Stack.Navigator>
+  );
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,51 @@
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+//
+// module.exports = {
+//   entry: './src/browser/index.web.js',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'dist'),
+//   },
+//   resolve: {
+//     alias: {
+//       'react-native$': 'react-native-web',
+//     },
+//     extensions: ['.web.js', '.js', '.tsx', '.ts', '.jsx', '.json']
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx|ts|tsx)$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: ['module:metro-react-native-babel-preset'],
+//             plugins: ['react-native-web'],
+//           },
+//         },
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: './public/index.html',
+//     }),
+//   ],
+//   devServer: {
+//     static: {
+//       directory: path.join(__dirname, 'public'),
+//     },
+//     hot: true,
+//   },
+// };
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.web.js',
+  entry: './src/browser/index.web.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -25,6 +68,32 @@ module.exports = {
             plugins: ['react-native-web'],
           },
         },
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: 'assets/images',
+              esModule: false,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|otf|woff|woff2|eot)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: 'assets/fonts',
+              esModule: false,
+            },
+          },
+        ],
       },
     ],
   },
